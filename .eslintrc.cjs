adc1e1ab59b313b16cf199be504c80e102afa26e
Juan282007 <juanpablogomezperdomo@gmail.com>
2026-04-26 18:43:15 -0500

    new fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,6 +11,6 @@ module.exports = {
   plugins: ['react-hooks', '@typescript-eslint'],
   rules: {
     '@typescript-eslint/no-unused-vars': 'warn',
-    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off',
   },
 }
